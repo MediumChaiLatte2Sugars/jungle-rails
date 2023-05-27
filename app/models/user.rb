06b1541207bd45b339e3d5309d56f8b906220e31
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-27 03:39:20 -0400

    Feat: add password min. length + format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :password, length: { minimum: 9 }
+  validates :password, format: { with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+    message: "must contain at least one digit, one lowercase letter, one uppercase letter, and one symbol" }
   validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :first_name, presence: true
   validates :last_name, presence: true
