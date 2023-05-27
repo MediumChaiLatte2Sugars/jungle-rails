77ce3d2572dcc434787c509523e0de9adb3c012f
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-27 03:21:10 -0400

    Test: define validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,82 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
     it 'verifies a user is created a with matching password and password_confirmation fields' do
-      pending 'in progress'
+      @user = User.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@example.com',
+        password: 'password',
+        password_confirmation: 'word'
+      })
+
+      @user.valid?
+
+      expect(@user.errors[:password_confirmation]).to include("doesn't match Password")
+      
     end
 
     it 'verifies the presence of email' do
-      pending 'in progress'
+      @user = User.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email: nil,
+        password: 'password',
+        password_confirmation: 'password'
+      })
+
+      @user.valid?
+
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
     it 'verifies the uniqueness of emails (case insensitive)' do
-      pending 'in progress'
+      @user = User.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'email@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      })
+
+      @user2 = User.create({
+        first_name: 'John',
+        last_name: 'Doe',
+        email: @user.email.upcase,
+        password: 'password',
+        password_confirmation: 'password'
+      })
+
+      @user2.valid?
+
+      expect(@user2.errors[:email]).to include("has already been taken")
     end
 
     it 'verifies the presence of a first name' do
-      pending 'in progress'
+      @user = User.create({
+        first_name: nil,
+        last_name: 'Doe',
+        email: 'email@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      })
+
+      @user.valid?
+      
+      expect(@user.errors[:first_name]).to include("can't be blank")
     end
 
     it 'verifies the presence of a last name' do
-      pending 'in progress'
+      @user = User.create({
+        first_name: 'John',
+        last_name: nil,
+        email: 'email@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      })
+
+      @user.valid?
+      
+      expect(@user.errors[:last_name]).to include("can't be blank")
     end
   end
 end
