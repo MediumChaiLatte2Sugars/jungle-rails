308137911a1a59542362fb9cd53723c4b537005f
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2023-05-27 05:01:52 -0400

    Feat: define class method for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def self.authenticate_with_credentials email, password
+    # Find the user with the provided email (case-insensitive)
+    user = User.find_by(email: email.downcase)
+
+    # Check if the user exists and the password is correct
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
